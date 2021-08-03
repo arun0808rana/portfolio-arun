73a06ad07f756a0edde0091330758a1c47cbe40a
dev001 <>
2021-08-04 01:08:46 +0530

    add:skills

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -1,51 +1,109 @@
 import React from "react";
 import styled from "styled-components/macro";
 import skillsBanner from "../../assets/images/bro.svg";
+import figma from "../../assets/images/figma.svg";
+import react from "../../assets/images/react.svg";
+import redux from "../../assets/images/redux.svg";
+import angular from "../../assets/images/ng.svg";
+import sass from "../../assets/images/sass.svg";
+import storybook from "../../assets/images/storybook.svg";
+import tailwind from "../../assets/images/tailwind.svg";
+import xd from "../../assets/images/xd.svg";
+import jsImg from "../../assets/images/js.svg";
+import cssImg from "../../assets/images/css.svg";
+import htmlImg from "../../assets/images/html.svg";
+import bootstrap from "../../assets/images/bootstrap.svg";
+import photoshop from "../../assets/images/photoshop.svg";
+import git from "../../assets/images/git.svg";
 
 function Skills() {
   return (
-    <Wrapper>
-        <div class="section-heading">Skills</div>
-      <div class="d-flex">
+    <>
+      <div class="section-heading">Skills</div>
+      <Wrapper>
         <div class="lhs">
-          <div class="blobs-container">
-            <div class="blob white"></div>
-            <div class="blob red"></div>
-            <div class="blob orange"></div>
-            <div class="blob yellow"></div>
-            <div class="blob blue"></div>
-            <div class="blob green"></div>
-            <div class="blob purple"></div>
-            <div class="blob"></div>
+          <div class="blobs-container d-flex">
+            <div class="blob white">
+              <img src={figma} alt="" />
+            </div>
+            <div class="blob red">
+              <img src={react} alt="" />
+            </div>
+            <div class="blob orange">
+              <img src={redux} alt="" />
+            </div>
+            <div class="blob yellow">
+              <img src={angular} alt="" />
+            </div>
+            <div class="blob blue">
+              <img src={sass} alt="" />
+            </div>
+            <div class="blob green">
+              <img src={storybook} alt="" />
+            </div>
+            <div class="blob purple">
+              <img src={bootstrap} alt="" />
+            </div>
+            <div class="blob">
+              <img src={cssImg} alt="" />
+            </div>
+            <div class="blob">
+              <img src={git} alt="" />
+            </div>
+            <div class="blob">
+              <img src={jsImg} alt="" />
+            </div>
+            <div class="blob">
+              <img src={tailwind} alt="" />
+            </div>
+            <div class="blob">
+              <img src={photoshop} alt="" />
+            </div>
+            <div class="blob">
+              <img src={htmlImg} alt="" />
+            </div>
           </div>
         </div>
         <div class="rhs">
           <img src={skillsBanner} alt="skills" className="skills-banner" />
         </div>
-      </div>
-    </Wrapper>
+      </Wrapper>
+    </>
   );
 }
 
 export default Skills;
 
 const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   .lhs {
+    .blobs-container {
+      /* justify-content: space-between; */
+      flex-wrap: wrap;
+      max-width: 576px;
+    }
   }
 
   .rhs {
     img {
       max-width: 100%;
+      height: 50vh;
     }
   }
   .blob {
     border-radius: 50%;
     box-shadow: 0 0 0 0 rgba(0, 0, 0, 1);
-    margin: 10px;
-    height: 20px;
-    width: 20px;
+    margin: 2rem;
+    height: 70px;
+    width: 70px;
     transform: scale(1);
     animation: pulse-black 2s infinite;
+    img {
+      height: 70px;
+      max-width: 100%;
+    }
   }
 
   @keyframes pulse-black {
@@ -66,7 +124,7 @@ const Wrapper = styled.div`
   }
 
   .blob.white {
-    background: white;
+    background: var(--primary-bg);
     box-shadow: 0 0 0 0 rgba(255, 255, 255, 1);
     animation: pulse-white 2s infinite;
   }
@@ -89,53 +147,53 @@ const Wrapper = styled.div`
   }
 
   .blob.red {
-    background: rgba(255, 82, 82, 1);
-    box-shadow: 0 0 0 0 rgba(255, 82, 82, 1);
+    background: var(--primary-bg);
+    box-shadow: 0 0 0 0 rgba(147, 129, 255, 1);
     animation: pulse-red 2s infinite;
   }
 
   @keyframes pulse-red {
     0% {
       transform: scale(0.95);
-      box-shadow: 0 0 0 0 rgba(255, 82, 82, 0.7);
+      box-shadow: 0 0 0 0 rgba(147, 129, 255, 0.7);
     }
 
     70% {
       transform: scale(1);
-      box-shadow: 0 0 0 10px rgba(255, 82, 82, 0);
+      box-shadow: 0 0 0 10px rgba(147, 129, 255, 0);
     }
 
     100% {
       transform: scale(0.95);
-      box-shadow: 0 0 0 0 rgba(255, 82, 82, 0);
+      box-shadow: 0 0 0 0 rgba(147, 129, 255, 0);
     }
   }
 
   .blob.orange {
-    background: rgba(255, 121, 63, 1);
-    box-shadow: 0 0 0 0 rgba(255, 121, 63, 1);
+    background: var(--primary-bg);
+    box-shadow: 0 0 0 0 rgba(184, 184, 255, 1);
     animation: pulse-orange 2s infinite;
   }
 
   @keyframes pulse-orange {
     0% {
       transform: scale(0.95);
-      box-shadow: 0 0 0 0 rgba(255, 121, 63, 0.7);
+      box-shadow: 0 0 0 0 rgba(184, 184, 255, 0.7);
     }
 
     70% {
       transform: scale(1);
-      box-shadow: 0 0 0 10px rgba(255, 121, 63, 0);
+      box-shadow: 0 0 0 10px rgba(184, 184, 255, 0);
     }
 
     100% {
       transform: scale(0.95);
-      box-shadow: 0 0 0 0 rgba(255, 121, 63, 0);
+      box-shadow: 0 0 0 0 rgba(184, 184, 255, 0);
     }
   }
 
   .blob.yellow {
-    background: rgba(255, 177, 66, 1);
+    background: var(--primary-bg);
     box-shadow: 0 0 0 0 rgba(255, 177, 66, 1);
     animation: pulse-yellow 2s infinite;
   }
@@ -158,7 +216,7 @@ const Wrapper = styled.div`
   }
 
   .blob.blue {
-    background: rgba(52, 172, 224, 1);
+    background: var(--primary-bg);
     box-shadow: 0 0 0 0 rgba(52, 172, 224, 1);
     animation: pulse-blue 2s infinite;
   }
@@ -181,7 +239,7 @@ const Wrapper = styled.div`
   }
 
   .blob.green {
-    background: rgba(51, 217, 178, 1);
+    background: var(--primary-bg);
     box-shadow: 0 0 0 0 rgba(51, 217, 178, 1);
     animation: pulse-green 2s infinite;
   }
@@ -204,7 +262,7 @@ const Wrapper = styled.div`
   }
 
   .blob.purple {
-    background: rgba(142, 68, 173, 1);
+    background: var(--primary-bg);
     box-shadow: 0 0 0 0 rgba(142, 68, 173, 1);
     animation: pulse-purple 2s infinite;
   }
